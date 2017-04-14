1c138f25a6734f8082b3879317c0fd3c8058ed5d
Gregory Brunner <gbrunner@esri.com>
2017-04-14 14:39:32 -0500

    updating script for dennis

--- season_stats.py
+++ season_stats.py
@@ -5,5 +5,9 @@ kd_stats = 'C:/Users/greg6750/Documents/IPython Notebooks/NBAproject/kd.json'
 with open(kd_stats) as data_file:
     data = json.load(data_file)
 
+print('Columns of the Data')
+print(data['resultSets'][0]['headers'])
+
+print('Game Stats')
 for row in data['resultSets'][0]['rowSet']:
     print(row)
